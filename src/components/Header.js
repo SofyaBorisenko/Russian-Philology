9d5b51988f010bf1d84539eb06d37570313d54a7
Sofya Borisenko <sofyaborisenko806@gmail.com>
2023-07-21 13:52:25 +0100

    finished Header with media queries

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
-import './styles.css';
+import './Header.css';
 import logo1 from '../images/logo1.png';
-import { Divider } from '@chakra-ui/react'
+import logo2 from '../images/logo2.png';
+import { extendTheme, Divider, Menu, MenuButton, MenuList, MenuItem, IconButton } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
 
+const breakpoints = {
+  sm: '30em', // 480px
+  md: '48em', // 768px
+  lg: '62em', // 992px
+  xl: '80em', // 1280px
+  '2xl': '96em', // 1536px
+}
+
+const theme = extendTheme({ breakpoints })
 const Header = () => {
     const [prevScrollPos, setPrevScrollPos] = useState(0);
     const [visible, setVisible] = useState(true);
     const headerRef = useRef(null);
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const handleMobileMenuToggle = () => {
+      setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
 
     const handleScroll = () => {
       const currentScrollPos = window.pageYOffset;
@@ -22,10 +38,61 @@ const Header = () => {
 
     return (
         <header className={`header ${visible ? '' : 'hidden'}`} ref={headerRef}>
-        <div className="header-top">
+        {/* Mobile Version */}
+        <div className="mobile-header">
+          <div class="header-container">
+            <div class="mobile-menu">
+              <Menu>
+                <MenuButton
+                  as={IconButton}
+                  aria-label='Options'
+                  icon={<HamburgerIcon />}
+                  variant='outline'
+                  onClick={handleMobileMenuToggle}
+                />
+                {isMobileMenuOpen && (
+                  <MenuList className="menu">
+                    <MenuItem as='a' href='#'>Главная</MenuItem>
+                    <MenuItem as='a' href='#'>Исследования</MenuItem>
+                    <MenuItem as='a' href='#'>Сотрудники</MenuItem>
+                    <MenuItem as='a' href='#'>Наши учителя</MenuItem>
+                    <MenuItem as='a' href='#'>Новости</MenuItem>
+                    <MenuItem as='a' href='#'>Фотогалерея</MenuItem>
+                    <MenuItem as='a' href='#'>Контакты</MenuItem>
+                  </MenuList>
+              )}
+            </Menu>
+            </div>
+            <h1 class="header-title">Русская Филология</h1>
+            <img
+            class="logo2"
+            src={logo2}
+            alt="Знак пси"
+            height='auto'
+            />
+          </div>
+          <div class="header-text-container">
+            <h2 class="header-subtitle">Лаборатория экопсихологии развития и психодидактики</h2>
+            <h3 class="header-caption">Группа психологических основ разработки школьных учебников</h3>
+          </div>
+        </div>
+        <Divider
+              className="divider"
+              orientation='horizontal'
+              display={{md: 'none', sm: 'block', base: 'block'}}
+              width='100%'
+              height='1.5px'
+              background='#FE205A'
+          />
+        {/* Full-Screen Version */}
+        <div className="full-screen-header">
             <div className="left-content">
                 <h1>Русская Филология</h1>
-                <img src={logo1} alt="Логотип ПИ РАО" style={{ width: '340px', height: '54px' }} />
+                <img className="logo1"
+                src={logo1}
+                alt="Логотип ПИ РАО"
+                style={{ width: '21.25rem', height: 'auto' }}
+                />
             </div>
             <div className="right-content">
                 <h2>Лаборатория экопсихологии развития и психодидактики</h2>
@@ -34,19 +101,24 @@ const Header = () => {
             </div>
         </div>
         <Divider
-            orientation='horizontal'
-            style={{ width: '84vw', height: '1.5px', background: '#FE205A', margin: '10px 8vw' }}
+          className="divider"
+          orientation='horizontal'
+          display={{md: 'flex', sm: 'none', base: 'none'}}
+          width='84vw'
+          height='1.5px'
+          background='#FE205A'
+          margin='0.625rem 8vw'
         />
-        <nav>
-            <ul className="menu">
-                <li><a href="/">Главная</a></li>
-                <li><a href="/research">Исследования</a></li>
-                <li><a href="/staff">Сотрудники</a></li>
-                <li><a href="/teachers">Наши учителя</a></li>
-                <li><a href="/news">Новости</a></li>
-                <li><a href="/photos">Фотогалерея</a></li>
-                <li><a href="/contacts">Контакты</a></li>
-            </ul>
+        <nav className="full-screen-menu">
+          <ul className="menu">
+            <li><a href="/">Главная</a></li>
+            <li><a href="/research">Исследования</a></li>
+            <li><a href="/staff">Сотрудники</a></li>
+            <li><a href="/teachers">Наши учителя</a></li>
+            <li><a href="/news">Новости</a></li>
+            <li><a href="/photos">Фотогалерея</a></li>
+            <li><a href="/contacts">Контакты</a></li>
+          </ul>
         </nav>
       </header>
     );

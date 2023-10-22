e9bf7cea420fadd57d349e250a41a39a144ec05d
Sofya <42944296+SofyaBorisenko@users.noreply.github.com>
2023-10-22 22:20:43 +0100

    upd tab name and some css

--- src/components/pages/Research/index.jsx
+++ src/components/pages/Research/index.jsx
@@ -13,51 +13,48 @@ function Research() {
   return (
     <div>
       <Header />
-        <main>
-          <Heading className="research-heading">Направления исследований</Heading>
-          <Tabs isFitted
-            position="relative"
-            variant={{ sm: "enclosed" }}
-          >
-            <TabList
+      <main>
+        <Heading className="research-heading">Направления исследований</Heading>
+        <Tabs isFitted position="relative" variant={{ sm: "enclosed" }}>
+          <TabList
             gap={1}
-            flexDirection={{ base: 'column', sm: 'row' }}
-            justifyContent={'flex-start'}
-            >
-              {[
-                'Школьный учебник на бумажном носителе',
-                'Цифровой школьный учебник',
-                'Чтение и понимание текста',
-                'Школьное литературоведение',
-              ].map((label, index) => (
-                <Tab
-                  key={index}
-                  className="research-dir-tab"
-                  borderTopRadius="0.375rem"
-                  borderBottomRadius={{ base: '0.375rem', sm: 'none' }}
-                  width={{ base: '100%', sm: 'fit-content' }}
-                  _selected={{ color: '#121d47', bg: '#C4EAA5' }}
-                >
-                  {label}
-                </Tab>
-              ))}
-            </TabList>
-            <TabPanels>
-              <TabPanel style={{ padding: '0.5rem 0 0 0' }}>
-                <PaperTextbook />
-              </TabPanel>
-              <TabPanel style={{ padding: '0.5rem 0 0 0' }}>
-                <DigitalTextbook />
-              </TabPanel>
-              <TabPanel style={{ padding: '0.5rem 0 0 0' }}>
-                <Reading />
-              </TabPanel>
-              <TabPanel style={{ padding: '0.5rem 0 0 0' }}>
-                <LiteraryCriticism />
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
-        </main>
+            flexDirection={{ base: "column", sm: "row" }}
+            justifyContent={"flex-start"}
+          >
+            {[
+              "Школьный учебник на бумажном носителе",
+              "Цифровой школьный учебник",
+              "Чтение и понимание текста",
+              "Школьное литературоведение на психологической основе",
+            ].map((label, index) => (
+              <Tab
+                key={index}
+                className="research-dir-tab"
+                borderTopRadius="0.375rem"
+                borderBottomRadius={{ base: "0.375rem", sm: "none" }}
+                width={{ base: "100%", sm: "fit-content" }}
+                _selected={{ color: "#121d47", bg: "#C4EAA5" }}
+              >
+                {label}
+              </Tab>
+            ))}
+          </TabList>
+          <TabPanels>
+            <TabPanel style={{ padding: "0.5rem 0 0 0" }}>
+              <PaperTextbook />
+            </TabPanel>
+            <TabPanel style={{ padding: "0.5rem 0 0 0" }}>
+              <DigitalTextbook />
+            </TabPanel>
+            <TabPanel style={{ padding: "0.5rem 0 0 0" }}>
+              <Reading />
+            </TabPanel>
+            <TabPanel style={{ padding: "0.5rem 0 0 0" }}>
+              <LiteraryCriticism />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </main>
       <Footer />
     </div>
   );

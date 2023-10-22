c1ec325b050da89f703cad7156ed0f2fdbb1cbf8
Sofya <42944296+SofyaBorisenko@users.noreply.github.com>
2023-10-22 22:17:50 +0100

    updated address

--- src/components/pages/Contacts/index.jsx
+++ src/components/pages/Contacts/index.jsx
@@ -16,25 +16,27 @@ function Contacts() {
           <div className="contact">
             <h2>Борисенко Наталья Анатольевна</h2>
             <p>
-              <FontAwesomeIcon icon={faEnvelope} />{' '}
-              <a href="mailto:borisenko_natalya@list.ru">borisenko_natalya@list.ru</a>
+              <FontAwesomeIcon icon={faEnvelope} />{" "}
+              <a href="mailto:borisenko_natalya@list.ru">
+                borisenko_natalya@list.ru
+              </a>
             </p>
             <p>
-              <FontAwesomeIcon icon={faPhone} />{' '}
+              <FontAwesomeIcon icon={faPhone} />{" "}
               <a href="tel:+7-909-692-11-14">+7-909-692-11-14</a>
             </p>
           </div>
           <div className="contact">
             <h2>Миронова Ксения Вадимовна</h2>
             <p>
-              <FontAwesomeIcon icon={faEnvelope} />{' '}
+              <FontAwesomeIcon icon={faEnvelope} />{" "}
               <a href="mailto:kseniamir@inbox.ru">kseniamir@inbox.ru</a>
             </p>
           </div>
           <div className="contact">
             <h2>Шишкова Светлана Викторовна</h2>
             <p>
-              <FontAwesomeIcon icon={faEnvelope} />{' '}
+              <FontAwesomeIcon icon={faEnvelope} />{" "}
               <a href="mailto:sshishkova@yandex.ru">sshishkova@yandex.ru</a>
             </p>
           </div>
@@ -42,9 +44,9 @@ function Contacts() {
         <div className="contact">
           <h2>Наш адрес:</h2>
           <p>
-            <FontAwesomeIcon icon={faMapMarker} />{' '}
-            125009, г. Москва, ул. Моховая, д. 9, строение 4, 
-            группа школьных учебников – к. 018 (в цоколе)
+            <FontAwesomeIcon icon={faMapMarker} /> 125009, г. Москва, ул.
+            Моховая, д. 9, строение 4, группа школьных учебников – Ц18
+            (Цокольный этаж)
           </p>
         </div>
         <Location />

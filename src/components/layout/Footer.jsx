4c1e7a89d1cd34d877c798c3e142cb903db9df9d
SofyaBorisenko <sofyaborisenko806@gmail.com>
2024-02-27 16:17:19 +0300

    modified footer styles, deleted address from footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,29 +1,42 @@
-import './Footer.scss';
-import { Divider } from "@chakra-ui/react";
+import { Divider, Image, VStack, Text } from '@chakra-ui/react';
 import logo3 from './assets/logo3.png';
 
 const Footer = () => {
   return (
-        <footer>
-          <Divider
-            orientation='horizontal'
-            style={{ width: '100%', height: '1.5px', background: '#FE205A', marginTop: '15px' }}
-          />
-          <div className="footer-top">
-            <img src={logo3} alt="Логотип ПИ РАО" className="logo3" />
-            <div className="footer-right">
-              <p>Адрес лаборатории:<br />
-              125009, г. Москва,<br />
-              ул. Моховая, д. 9,<br />
-              стр. 4, к. 018</p>
-            </div>
-          </div>
-          <div className="footer-bottom">
-            <p>© Русская филология, 2023. Все права защищены.<br />
-              При использовании любых материалов сайта, включая графику и тексты, <br />
-              активная ссылка на сайт обязательна.</p>
-          </div>
-        </footer>
+    <footer style={{ backgroundColor: '#f0f6f1' }}>
+      <Divider
+        orientation='horizontal'
+        width={['100%', null, '80vw', '64vw']}
+        height='1.5px'
+        background='#FE205A'
+        mx={['none', null, '10vw', '18vw']}
+        mt={['1rem', '1.5rem', null, '2rem']}
+      />
+      <VStack py={['0.5rem', '0.625rem', '0.75rem', '1rem', '2rem']}>
+        <Image
+          src={logo3}
+          alt='Логотип ПИ РАО'
+          maxW={['4rem', '5rem', '5.25rem', '5.5rem', '6rem']}
+          h='auto'
+          flexShrink='0'
+        />
+        <Text
+          color='#121d47'
+          align='center'
+          pt={['0.5rem', null, '0.75rem', null, '1rem']}
+          fontFamily='PT Serif'
+          fontStyle='normal'
+          fontWeight='700'
+          fontSize={['0.625rem', null, '0.725rem', '0.8rem', '0.9rem']}
+        >
+          © Русская филология, 2023. Все права защищены.
+          <br />
+          При использовании любых материалов сайта, включая графику и тексты,{' '}
+          <br />
+          активная ссылка на rus-philology.ru обязательна.
+        </Text>
+      </VStack>
+    </footer>
   );
 };
 export default Footer;
